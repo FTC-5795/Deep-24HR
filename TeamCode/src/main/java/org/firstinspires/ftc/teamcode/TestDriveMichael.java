582549a157e4a3fbbcd436f29e8ffc5e4f02879b
mdanley1234 <112212314+mdanley1234@users.noreply.github.com>
2024-09-08 11:12:27 -0400

    Fixed Mecanum whatever

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveMichael.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveMichael.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class TestDriveMichael extends LinearOpMode {
@@ -17,9 +19,14 @@ public class TestDriveMichael extends LinearOpMode {
         backRight = hardwareMap.get(DcMotorEx.class, "backRight");
 
         frontLeft.setDirection(DcMotorEx.Direction.FORWARD);
-        frontRight.setDirection(DcMotorEx.Direction.FORWARD);
+        frontRight.setDirection(DcMotorEx.Direction.REVERSE);
         backLeft.setDirection(DcMotorEx.Direction.FORWARD);
-        backRight.setDirection(DcMotorEx.Direction.FORWARD);
+        backRight.setDirection(DcMotorEx.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         double y, x, rx;
         double fLPower, fRPower, bLPower, bRPower;
@@ -32,10 +39,10 @@ public class TestDriveMichael extends LinearOpMode {
             y = -gamepad1.left_stick_y;
             rx = gamepad1.right_stick_x;
 
-            fLPower = y + rx + x;
-            fRPower = y - rx + x;
-            bLPower = y + rx - x;
-            bRPower = y - rx - x;
+            fLPower = y + x + rx;
+            fRPower = y - x - rx;
+            bLPower = y - x + rx;
+            bRPower = y + x - rx;
 
             divisor = Math.max(Math.max(Math.abs(fLPower), Math.abs(fRPower)), Math.max(Math.abs(bLPower), Math.abs(bRPower)));
             divisor = Math.max(divisor, 1);
